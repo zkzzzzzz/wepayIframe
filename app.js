618b863e5e2db7bf21a6e5f6085488e4a5054f52
Jiawei Zhang <809681148@qq.com>
2021-05-03 21:54:07 -0700

    fix port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 let fs = require('fs');
 
 const app = express();
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT || 5000;
 app.use(express.static('src'))
 
 
